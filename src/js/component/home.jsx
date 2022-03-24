2fc8f3cb5598510387f7a07a1c09b674e3cf340d
FrankLobo <frankeloboq@gmail.com>
2022-03-24 20:46:36 +0000

    en desarrollo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("");
+
+	const validateInput = (event) => {
+		if (inputValue === "") {
+			alert("The input cannot be empty");
+		}
+		if (event.key === "ENTER") {
+			return;
+		}
+	};
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="main-container">
+			<h1 className="text-center mt-5">TODOS</h1>
+			<div className="card-of-list">
+				<input
+					type="text"
+					className="add-todo"
+					placeholder="add a new to do"
+					onChange={(e) => {
+						setInputValue(e.target.value);
+					}}
+					value={inputValue}
+					onKeyPress={inputValue}
+				/>
+				<ul className="group-list">
+					{inputValue !== "" ? (
+						<li className="items">
+							{setInputValue(e.target.value)}
+							<button className="delete-item">X</button>
+						</li>
+					) : (
+						alert("The input cannot be empty")
+					)}
+				</ul>
+			</div>
 		</div>
 	);
 };
